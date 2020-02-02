6abdf110b7726efb6f7442dca076bf12efaf82ab
Ewa Czech <ewa.czech@hotmail.com>
2020-02-02 23:07:29 +0100

    add comment functionality

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -7,7 +7,7 @@ const CommentInput = props => {
 				type="text"
 				value={props.comment}
 				placeholder="Add comment... "
-				onChange={props.changeComment}
+				onChange={e => props.setCommentInput(e.target.value)}
 			/>
 		</form>
 	);
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,15 +6,31 @@ import './Comment.css';
 
 const CommentSection = props => {
 	const [comment, setComment] = useState(props.comments);
-
+	const [commentInput, setCommentInput] = useState('');
 	// Add state for the comments
+
+	const addComment = e => {
+		e.preventDefault();
+		setComment([
+			...comment,
+			{
+				username: 'ewa',
+				text: commentInput
+			}
+		]);
+	};
+
 	return (
 		<div>
 			{comment.map((data, index) => {
 				return <Comment key={index} comment={data} />;
 			})}
 			{/* map through the comments data and return the Comment component */}
-			<CommentInput />
+			<CommentInput
+				setCommentInput={setCommentInput}
+				comment={commentInput}
+				submitComment={addComment}
+			/>
 		</div>
 	);
 };
