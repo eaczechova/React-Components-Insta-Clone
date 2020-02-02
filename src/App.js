f2279d2b3032c0880ea1bdc62df72343170a48b0
Ewa Czech <ewa.czech@hotmail.com>
2020-02-02 14:07:08 +0100

    add filter to search bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import dummyData from './dummy-data.js';
 import SearchBar from './components/SearchBar/SearchBarContainer';
@@ -12,27 +12,15 @@ import PostsPage from './components/PostsContainer/PostsPage';
 import CommentSection from './components/CommentSection/CommentSectionContainer';
 
 const App = () => {
-	let initialDate = dummyData;
-	const [postData, setPostData] = useState(initialDate);
+	const [postData, setPostData] = useState(dummyData);
 	const [commentData, setCommentData] = useState(dummyData);
-
 	const [searchTerm, setSearchTerm] = useState('');
-
-	const filtered = postData.filter(search => {
-		if (search.username.indexOf(searchTerm) > -1) {
-			return search;
-		}
-	});
-
-	// setPostData(initialDate);
-	// console.log('filtered', filtered);
 
 	// set up state for the dummy data and pass to your PostsPage
 	return (
 		<div className="App">
 			<SearchBar search={setSearchTerm} />
 			<PostsPage postData={postData} searchTerm={searchTerm} />
-			{/* <CommentSection commentData={commentData} /> */}
 			{/* Add components here  and pass props where appropriate */}
 		</div>
 	);
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,17 +3,28 @@ import React from 'react';
 import Post from './Post';
 import './Posts.css';
 
-// console.log('post page', data.username);
-
 // pass the data from App.js down as props then map through the data
 
 const PostsPage = props => {
-	console.log(props.searchTerm);
+	const filteredPosts = props.postData.filter(data => {
+		if (data.username.includes(props.searchTerm)) {
+			return data;
+		}
+	});
+
 	const arrayOfPosts = props.postData.map((data, index) => {
 		return <Post key={index} post={data} />;
 	});
 
-	return <div className="posts-container-wrapper">{arrayOfPosts}</div>;
+	const arrayOfFilteredPosts = filteredPosts.map((data, index) => {
+		return <Post key={index} post={data} />;
+	});
+
+	return (
+		<div className="posts-container-wrapper">
+			{arrayOfPosts > arrayOfFilteredPosts ? arrayOfFilteredPosts : arrayOfPosts}
+		</div>
+	);
 };
 
 export default PostsPage;
